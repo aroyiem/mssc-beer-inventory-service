8e7c3832e6183f1ba731d6c477df955587e44629
Anirban Roy <aroyiem@gmail.com>
2021-02-25 15:06:56 +0000

    adding deallocate order action

--- src/main/java/com/roy/beer/inventory/service/config/JMSConfig.java
+++ src/main/java/com/roy/beer/inventory/service/config/JMSConfig.java
@@ -14,6 +14,7 @@ public class JMSConfig {
     public static final String NEW_INVENTORY_QUEUE = "new-inventory";
     public static final String ALLOCATE_ORDER_QUEUE = "allocate-order";
     public static final String ALLOCATE_ORDER_RESPONSE_QUEUE = "allocate-order-response";
+    public static final String DEALLOCATE_ORDER_QUEUE = "deallocate-order";
 
     @Bean // serialize message content to json using TestMessage
     public MessageConverter jacksonJmsMessageConverter(ObjectMapper objectMapper) {
